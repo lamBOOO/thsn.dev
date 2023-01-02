1483c3b053810c0aba73143a8173f55b73eccfba
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2023-01-02 11:58:44 +0100

    Fix missing key prop

--- pages/index.tsx
+++ pages/index.tsx
@@ -218,7 +218,7 @@ console.log('It works!')
                     "CIP stabilization"
                   ].map(
                     (a, b) =>
-                      (<span className="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-200 dark:text-gray-800">{a}</span>)
+                      (<span className="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-200 dark:text-gray-800" key={b}>{a}</span>)
                   )
                 }
               </div>
