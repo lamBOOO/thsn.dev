caf623a3e30cd60f20a8ad71dfe804da26adaab6
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2023-01-02 12:03:10 +0100

    Change contact btn

--- pages/index.tsx
+++ pages/index.tsx
@@ -132,7 +132,7 @@ console.log('It works!')
             />
           </div>
         </div>
-        <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Contact me!</button>
+        <button type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Contact me! 👋</button>
         <h2 id="research" className="scroll-mt-20">
           Research Interest & Projects
         </h2>
