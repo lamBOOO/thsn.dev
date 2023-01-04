d29afa12bc9cb0438eca7aee6e1a215978130781
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2023-01-04 16:56:32 +0100

    Add journal pubs

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,6 +23,13 @@ import plot1 from '../public/plot1.jpg'
 import plot2 from '../public/plot2.jpg'
 import sticker from '../public/sticker.png'
 
+import p2021_1 from '../public/p2021_1.jpg'
+import p2021_2 from '../public/p2021_2.jpg'
+import p2021_3 from '../public/p2021_3.jpg'
+import p2022_1 from '../public/p2022_1.jpg'
+import p2022_2 from '../public/p2022_2.jpg'
+import p2022_3 from '../public/p2022_3.jpg'
+
 export default function Home() {
   return (
     <>
@@ -87,7 +94,7 @@ export default function Home() {
               Research Interest & Projects
             </h2>
             <h3>Journal Publications</h3>
-            <div className="border flex flex-col justify-between h-full bg-white rounded-md p-4">
+            <div className="border flex flex-col justify-between h-full bg-white rounded-md p-4 my-2">
               <span className='leading-5 mb-2'>
                 <span className='font-bold'>
                   A Quasi-Optimal Factorization Preconditioner for Periodic Schrödinger Eigenstates in Anisotropically Expanding Domains
@@ -113,11 +120,23 @@ export default function Home() {
               </div>
               <div>
                 <p className='font-norma text-xs leading-4 text-justify my-2'>
+                <span className='float-right  ml-1 mb-1 border'>
                   <Image
-                    className="float-right sm:w-48 w-32 ml-1 mb-1 m-0"
-                    src={dd_teaser}
-                    alt="Picture of the author"
+                    className="w-40 m-0"
+                    src={p2022_1}
+                       alt="Picture of the author"
                   />
+                  <Image
+                    className="w-40 m-0"
+                    src={p2022_2}
+                       alt="Picture of the author"
+                  />
+                  <Image
+                    className="w-40 m-0"
+                    src={p2022_3}
+                       alt="Picture of the author"
+                  />
+                  </span>
                   This paper provides a provably quasi-optimal preconditioning strategy of the linear Schrödinger eigenvalue problem with periodic potentials for a possibly nonuniform spatial expansion of the domain. The quasi-optimality is achieved by having the iterative eigenvalue algorithms converge in a constant number of iterations for different domain sizes. In the analysis, we derive an analytic factorization of the spectrum and asymptotically describe it using concepts from the homogenization theory. This decomposition allows us to express the eigenpair as an easy-to-calculate cell problem solution combined with an asymptotically vanishing remainder. We then prove that the easy-to-calculate limit eigenvalue can be used in a shift-and-invert preconditioning strategy to bound the number of eigensolver iterations uniformly. Several numerical examples illustrate the effectiveness of this quasi-optimal preconditioning strategy.
                 </p>
                 <div>
@@ -139,7 +158,6 @@ export default function Home() {
                     )
                   }
                 </div>
-
                 <div className='flex flex-wrap gap-1 leading-none my-2'>
 
                   <Link href="https://doi.org/10.1137/21M1456005">
@@ -170,7 +188,104 @@ export default function Home() {
                     </span>
                   </Link>
                 </div>
+              </div>
+            </div>
 
+
+
+            <div className="border flex flex-col justify-between h-full bg-white rounded-md p-4 my-2">
+              <span className='leading-5 mb-2'>
+                <span className='font-bold'>
+                  fenicsR13: A Tensorial Mixed Finite Element Solver for theLinear R13 Equations Using the FEniCS Computing Platform
+                </span>
+                <br />
+                <i className="text-xs">
+                  ACM Trans. Math. Softw. 47, 2, Article 17 (April 2021)
+                </i>
+              </span>
+              <div className='m-0 flex flex-wrap gap-1 mb-1'>
+                <span className="bg-gray-100 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded">
+                  <svg aria-hidden="true" className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd"></path></svg>
+                  04/2021
+                </span>
+                <span className="bg-red-100 text-red-800 text-xs font-medium px-2.5 py-0.5 rounded border-red-500">
+                  <span className="pr-1"><FontAwesomeIcon icon={faPerson} /></span>
+                  Lambert Theisen
+                </span>
+                <span className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded border-blue-500"><Link href="https://www.acom.rwth-aachen.de/5people/torrilhon/start">
+                  <span className="pr-1"><FontAwesomeIcon icon={faPerson} /></span>
+                  Manuel Torrilhon
+                </ Link></span>
+              </div>
+              <div>
+                <p className='font-norma text-xs leading-4 text-justify my-2'>
+                  <span className='float-right ml-1 mb-1 border'>
+                  <Image
+                    className="w-40 m-0"
+                    src={p2021_1}
+                       alt="Picture of the author"
+                  />
+                  <Image
+                    className="w-40 m-0"
+                    src={p2021_2}
+                       alt="Picture of the author"
+                  />
+                  <Image
+                    className="w-40 m-0"
+                    src={p2021_3}
+                       alt="Picture of the author"
+                  />
+                  </span>
+                  We present a mixed finite element solver for the linearized regularized 13-moment equations of non-equilibrium gas dynamics. The Python implementation builds upon the software tools provided by the FEniCS computing platform. We describe a new tensorial approach utilizing the extension capabilities of FEniCS’ Unified Form Language to define required differential operators for tensors above second degree. The presented solver serves as an example for implementing tensorial variational formulations in FEniCS, for which the documentation and literature seem to be very sparse. Using the software abstraction levels provided by the Unified Form Language allows an almost one-to-one correspondence between the underlying mathematics and the resulting source code. Test cases support the correctness of the proposed method using validation with exact solutions. To justify the usage of extended gas flow models, we discuss typical application cases involving rarefaction effects. We provide the documented and validated solver publicly.
+                </p>
+                <div>
+                </div>
+
+                <div className='text-sm mb-2'>
+                  <span className='text-xs'>Keywords: </span>
+                  {
+                    [
+                      "tensorial mixed finite element method",
+                      "R13 equations",
+                      "FEniCS project",
+                      "continuous interior penalty"
+                    ].map(
+                      (a, b) =>
+                        (<span className="bg-gray-50 text-gray-800 text-xs font-medium mr-1 py-0 rounded-sm" key={b}># {a}</span>)
+                    )
+                  }
+                </div>
+
+                <div className='flex flex-wrap gap-1 leading-none my-2'>
+
+                  <Link href="https://doi.org/10.1145/3442378">
+                    <span className="border-gray-500 hover:border-black">
+                      <span className='border-inherit border-t border-l border-b text-xs font-mono inline-flex items-center rounded-tl rounded-bl px-2 py-0.5 bg-gray-300 text-gray-800 font-bold'>
+                        <span className="pr-1"><i className="ai ai-doi"></i></span>
+                        DOI
+                      </span>
+                      <span className='border-inherit border-t border-r border-b text-xs inline-flex items-center rounded-tr rounded-br px-2 py-0.5 bg-gray-200 text-gray-800 font-mono'>10.1145/3442378</span>
+                    </span>
+                  </Link>
+                  <Link className="m-0" href="https://arxiv.org/abs/2007.05944">
+                    <span className="border-gray-500">
+                      <span className='border-inherit border-t border-l border-b text-xs font-mono inline-flex items-center rounded-tl rounded-bl px-2 py-0.5 bg-red-300 text-red-800 font-bold '>
+                        <span className="pr-1"><i className="ai ai-arxiv"></i></span>
+                        arXiv
+                      </span>
+                      <span className='border-inherit border-t border-r border-b text-xs inline-flex items-center rounded-tr rounded-br px-2 py-0.5 bg-red-200 text-red-800 font-mono decoration-red-700'>2007.05944</span>
+                    </span>
+                  </Link>
+                  <Link href="https://doi.org/10.5281/zenodo.4172951">
+                    <span className="border-gray-500">
+                      <span className='border-inherit border-t border-l border-b text-xs font-mono inline-flex items-center rounded-tl rounded-bl px-2 py-0.5 bg-blue-300 text-blue-800 font-bold'>
+                        <span className="pr-1"><i className="ai ai-zenodo"></i></span>
+                        Zenodo
+                      </span>
+                      <span className='border-inherit border-t border-r border-b text-xs inline-flex items-center rounded-tr rounded-br px-2 py-0.5 bg-blue-200 text-blue-800 font-mono'>10.5281/zenodo.4172951</span>
+                    </span>
+                  </Link>
+                </div>
               </div>
             </div>
             <h3>Projects</h3>
