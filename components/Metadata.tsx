aaf118a204c979c2f6bf83f422fa937caba4da5d
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2023-01-04 14:14:12 +0100

    Refactor Metadata

---
+++ components/Metadata.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Metadata() {
+  return (
+    <Head>
+      <title>Lambert Theisen - Computational Engineer, PhD Student & Digital Artist.</title>
+      <meta name="description" content="The personal website of Lambert Theisen containing his research, contact infos and a blog." />
+      <meta name="keywords" content="research, science, portfolio, personal webpage" />
+      <meta name="author" content="Lambert Theisen" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
