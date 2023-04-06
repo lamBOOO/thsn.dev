43bcb73935d9416b97c28b95906548fd3c16c178
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2023-04-06 14:10:05 +0200

    Fix nextjs config

--- next.config.js
+++ next.config.js
@@ -1,36 +1,19 @@
-
-
-
 /** @type {import('next').NextConfig} */
-
-// import remarkMath from "remark-math";
-// import rehypeKatex from "rehype-katex";
-
 const nextConfig = {
+  experimental: {
+    appDir: false,
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+};
 
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
     remarkPlugins: [],
     rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
   },
 })
-module.exports = withMDX({
-  // Append the default value with md extensions
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-})
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-module.exports = withBundleAnalyzer({})
+module.exports = withMDX(nextConfig)
