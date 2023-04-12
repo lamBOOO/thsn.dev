05ac92c8f8b6780ab689c72505e714b9cdceaafa
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2023-04-12 09:51:11 +0200

    Improve sitemap further with lastmod

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -2,25 +2,29 @@ import { getSortedPostsData } from '../lib/posts';
 
 const EXTERNAL_DATA_URL = "https://thsn.dev"
 
-function generateSiteMap(posts : any) {
+function generateSiteMap(posts: any) {
+  const today = new Date().toISOString().split('T')[0]
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <!--We manually set the two URLs we know already-->
      <url>
        <loc>https://thsn.dev</loc>
+       <lastmod>${today}</lastmod>
      </url>
      <url>
        <loc>https://thsn.dev/blog</loc>
+       <lastmod>${today}</lastmod>
      </url>
      ${posts
-       .map(({ id } : any) => {
-         return `
-       <url>
-           <loc>${`${EXTERNAL_DATA_URL}/blog/${id}`}</loc>
-       </url>
+      .map(({ id, date }: any) => {
+        return `
+      <url>
+        <loc>${`${EXTERNAL_DATA_URL}/blog/${id}`}</loc>
+        <lastmod>${date}</lastmod>
+      </url>
      `;
-       })
-       .join('')}
+      })
+      .join('')}
    </urlset>
  `;
 }
@@ -29,7 +33,7 @@ function SiteMap() {
   // getServerSideProps will do the heavy lifting
 }
 
-export async function getServerSideProps({ res } : any) {
+export async function getServerSideProps({ res }: any) {
   // We make an API call to gather the URLs for our site
   const allPostsData = getSortedPostsData();
 
