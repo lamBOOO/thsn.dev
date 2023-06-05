33ea7873e44ddadb9e251fc86dae01cb27dcf15a
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2023-06-05 21:03:38 +0200

    Add meta again

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,17 @@ export const metadata: Metadata = {
     locale: 'en-US',
     type: 'website',
   },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 }
 
 export default function RootLayout({
