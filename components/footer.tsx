b841eb598b51a73536e7fa87ddb481a0149ac1e9
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2023-08-03 11:23:19 +0200

    Add build time to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,12 +2,15 @@ import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 
 export default function Footer() {
+  const currentDate = new Date().toJSON();
   return (
     <>
       <hr className='border-0' />
       <footer className=" bg-white py-4 border-gray-100 border-t">
         <span className="px-4 block text-sm text-gray-500 text-center">
           © 2023 Lambert Theisen. All Rights Reserved.
+          <br></br>
+          <span className='text-xs text-gray-500 font-mono'>// build time: {currentDate}</span>
         </span>
       </footer>
     </>
